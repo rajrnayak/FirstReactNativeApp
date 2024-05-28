24a2784f575db544b31828f08069270df7e45457
raj nayak <155231280+rajrnayakwork@users.noreply.github.com>
2024-05-28 16:52:25 +0530

    complete CRUD in local storage

--- app/pages/todo-list/Form.jsx
+++ app/pages/todo-list/Form.jsx
@@ -1,26 +1,244 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable, Modal, TextInput } from "react-native";
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function Form() {
+const Form = forwardRef(function Form({ getAllData }, ref) {
+	const [modalVisible, setModalVisible] = useState(false);
+	const [date, setDate] = useState(new Date());
+	const [field, setField] = useState({
+		task: "first task",
+		description: "this is first one",
+		usage: "work",
+		date_time: "2024-05-28",
+	});
+
+	const openModal = (task) => {
+		task ? setField(task) : setField({ ...field, date_time: date });
+		setModalVisible(!modalVisible);
+	};
+
+	useImperativeHandle(ref, () => {
+		return {
+			openModal,
+			closeModal,
+		};
+	});
+
+	const onChange = (event, selectedDate) => {
+		const currentDate = selectedDate;
+		setDate(currentDate);
+		setField({ ...field, date_time: currentDate });
+	};
+
+	const showMode = (currentMode) => {
+		DateTimePickerAndroid.open({
+			value: date,
+			onChange,
+			mode: currentMode,
+			is24Hour: true,
+		});
+	};
+
+	const showDatePicker = () => {
+		showMode("date");
+	};
+
+	const showTimePicker = () => {
+		showMode("time");
+	};
+
+	const storeData = async (value) => {
+		try {
+			let tasks = await AsyncStorage.getItem("tasks");
+			tasks = tasks ? JSON.parse(tasks) : {};
+
+			value.id = !value.id ? Date.now() : value.id;
+			tasks[value.id.toString()] = JSON.stringify(value);
+			await AsyncStorage.setItem("tasks", JSON.stringify(tasks));
+			getAllData();
+		} catch (e) {
+			console.log(e);
+			console.log("its error in store method.");
+		}
+	};
+
+	const submit = () => {
+		storeData(field);
+		closeModal();
+	};
+
+	const closeModal = () => {
+		setModalVisible(!modalVisible);
+		setField({
+			id: "",
+			task: "",
+			description: "",
+			usage: "",
+			date_time: "",
+		});
+	};
+
 	return (
-		<></>
-		// <Modal
-		// 	animationType="slide"
-		// 	transparent={true}
-		// 	visible={modalVisible}
-		// 	onRequestClose={() => {
-		// 		setModalVisible(!modalVisible);
-		// 	}}>
-		// 	<View style={styles.centeredView}>
-		// 		<View style={styles.modalView}>
-		// 			<View></View>
-		// 			<Pressable style={[styles.button, { right: 90 }]} onPress={() => setModalVisible(!modalVisible)}>
-		// 				<Text style={styles.textStyle}>Submit</Text>
-		// 			</Pressable>
-		// 			<Pressable style={[styles.button, { backgroundColor: "lightgrey" }]} onPress={() => setModalVisible(!modalVisible)}>
-		// 				<Text style={[styles.textStyle, { color: "black" }]}>Cancel</Text>
-		// 			</Pressable>
-		// 		</View>
-		// 	</View>
-		// </Modal>
+		<Modal
+			animationType="slide"
+			transparent={true}
+			visible={modalVisible}
+			onRequestClose={() => {
+				setModalVisible(!modalVisible);
+			}}>
+			<View style={styles.centeredView}>
+				<View style={styles.modalView}>
+					<View style={styles.fieldBox}>
+						<Text style={styles.fieldTitle}>Task :</Text>
+						<TextInput
+							style={styles.textInput}
+							placeholder="What do you want to do?"
+							value={field.task}
+							onChangeText={(text) => {
+								setField({ ...field, task: text });
+							}}
+						/>
+					</View>
+					<View style={styles.fieldBox}>
+						<Text style={styles.fieldTitle}>Description :</Text>
+						<TextInput
+							editable
+							multiline
+							numberOfLines={4}
+							style={styles.textInput}
+							placeholder="Enter your description."
+							value={field.description}
+							onChangeText={(text) => {
+								setField({ ...field, description: text });
+							}}
+						/>
+					</View>
+					<View style={styles.fieldBox}>
+						<Text style={styles.fieldTitle}>Task For :</Text>
+						<Picker
+							style={styles.textInput}
+							selectedValue={field.usage}
+							onValueChange={(itemValue, itemIndex) => {
+								setField({ ...field, usage: itemValue });
+							}}>
+							<Picker.Item label="Personal" value="personal" />
+							<Picker.Item label="Work" value="work" />
+						</Picker>
+					</View>
+					<Text style={styles.fieldBox}>selected Date & Time : {field.date_time.toLocaleString()}</Text>
+					<View style={styles.fieldBox}>
+						<Text style={styles.fieldTitle}>Select Date :</Text>
+						<Pressable style={[styles.dateTimeButton]} onPress={showDatePicker}>
+							<Text style={{ textAlign: "center" }}>Select Date</Text>
+						</Pressable>
+					</View>
+					<View style={styles.fieldBox}>
+						<Text style={styles.fieldTitle}>Select Time :</Text>
+						<Pressable style={[styles.dateTimeButton]} onPress={showTimePicker}>
+							<Text style={{ textAlign: "center" }}>Select Time</Text>
+						</Pressable>
+					</View>
+					<Pressable style={[styles.button, { right: 90 }]} onPress={submit}>
+						<Text style={styles.textStyle}>Submit</Text>
+					</Pressable>
+					<Pressable style={[styles.button, { backgroundColor: "lightgrey" }]} onPress={closeModal}>
+						<Text style={[styles.textStyle, { color: "black" }]}>Cancel</Text>
+					</Pressable>
+				</View>
+			</View>
+		</Modal>
 	);
-}
+});
+
+export default Form;
+
+// else {
+// 	let tasksArr = [];
+
+// 	Object.keys(tasks).forEach(function (key) {
+// 		tasksArr.push(JSON.parse(tasks[key]));
+// 	});
+
+// 	tasksArr = tasksArr.map((row) => {
+// 		if (row.id == field.id) {
+// 			return field;
+// 		} else {
+// 			return row;
+// 		}
+// 	});
+
+// 	await AsyncStorage.setItem("tasks", tasksArr);
+// }
+
+const styles = StyleSheet.create({
+	centeredView: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 22,
+		height: "100%",
+	},
+
+	modalView: {
+		width: "98%",
+		height: "80%",
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 30,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+
+	button: {
+		padding: 10,
+		backgroundColor: "#2196F3",
+		alignItems: "center",
+		justifyContent: "center",
+		borderRadius: 20,
+		position: "absolute",
+		bottom: 20,
+		right: 15,
+	},
+
+	textStyle: {
+		color: "white",
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+
+	fieldTitle: {
+		color: "grey",
+		marginLeft: 5,
+		fontSize: 16,
+	},
+
+	textInput: {
+		backgroundColor: "#fff2e4",
+		padding: 8,
+		marginTop: 10,
+		marginBottom: 10,
+		borderColor: "grey",
+		borderRadius: 10,
+	},
+
+	fieldBox: {
+		marginBottom: 10,
+	},
+
+	dateTimeButton: {
+		backgroundColor: "#fff2e4",
+		padding: 10,
+		width: 130,
+		borderRadius: 10,
+		marginTop: 5,
+		marginBottom: 5,
+	},
+});
--- app/pages/todo-list/Index.jsx
+++ app/pages/todo-list/Index.jsx
@@ -1,36 +1,48 @@
-import { StyleSheet, Text, View, SafeAreaView, Pressable, Modal, TextInput, TextBase, Button, ScrollView, Alert, FlatList } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Pressable, ScrollView, FlatList, Button } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { useEffect, useState } from "react";
-import { Picker } from "@react-native-picker/picker";
-import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import { useEffect, useRef, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SQLite from "expo-sqlite";
+import Form from "./Form.jsx";
 
 export default function ToDoIndex({ navigation, route }) {
-	const [modalVisible, setModalVisible] = useState(false);
 	const [data, setData] = useState([]);
+
+	const modalRef = useRef(null);
 
 	useEffect(() => {
 		getAllData();
 	}, []);
 
-	useEffect(() => {
-		console.log("data showing");
-		console.log(data);
-	}, [data]);
-
 	const getAllData = async () => {
 		try {
 			const keys = await AsyncStorage.getAllKeys();
-			if (keys.length > 0) {
-				const items = await AsyncStorage.multiGet(keys);
-				let rows = items.reduce((accumulator, [key, value]) => {
-					accumulator[key] = JSON.parse(value);
-					return accumulator;
-				}, {});
-				setData(rows);
-			}
+			const rows = await AsyncStorage.multiGet(keys);
+
+			let tasks = await AsyncStorage.getItem("tasks");
+			tasks = tasks ? JSON.parse(tasks) : {};
+
+			let tasksArr = [];
+
+			Object.keys(tasks).forEach(function (key) {
+				tasksArr.push(JSON.parse(tasks[key]));
+			});
+
+			setData(tasksArr);
 		} catch (e) {
 			console.log("its error in get all data method.");
+		}
+	};
+
+	const destroyData = async (id) => {
+		try {
+			let tasks = await AsyncStorage.getItem("tasks");
+			tasks = tasks ? JSON.parse(tasks) : {};
+			delete tasks[id];
+			await AsyncStorage.setItem("tasks", JSON.stringify(tasks));
+			getAllData();
+		} catch (e) {
+			console.log("its error in destroy method.");
 		}
 	};
 
@@ -43,149 +55,45 @@ export default function ToDoIndex({ navigation, route }) {
 		}
 	};
 
+	const openModal = (task) => {
+		modalRef.current.openModal(task);
+	};
+
 	return (
 		<>
 			<SafeAreaView>
 				<View style={{ height: "100%" }}>
 					{/* <Button onPress={() => clearData()} title="button" /> */}
-					<View style={{ height: "90%", padding: 10 }}>{/* <View style={{ borderWidth: 2, borderColor: "#FF6400", borderRadius: 10, marginTop: 10, paddingBottom: 130 }}></View> */}</View>
-					<Pressable style={({ pressed }) => [pressed ? styles.floatingButton : styles.floatingButtonPressed]} onPress={() => setModalVisible(true)}>
+					<View style={{ height: "90%", padding: 10 }}>
+						<FlatList
+							data={data}
+							renderItem={({ item, index }) => (
+								<ScrollView>
+									<View style={{ borderWidth: 2, borderColor: "#FF6400", borderRadius: 10, marginTop: 10, padding: 20, gap: 5, backgroundColor: "white" }}>
+										<Text>Task: {item.id}</Text>
+										<Text>Task: {item.task}</Text>
+										<Text>Description : {item.description}</Text>
+										<Text>Task For : {item.usage}</Text>
+										<Text>Date & Time : {item.date_time}</Text>
+										<Pressable style={{ position: "absolute", bottom: 20, right: 60, backgroundColor: "skyblue", padding: 5, borderRadius: 40 }} onPress={() => openModal(item)}>
+											<Ionicons name="create-outline" size={23} />
+										</Pressable>
+
+										<Pressable style={{ position: "absolute", bottom: 20, right: 10, backgroundColor: "skyblue", padding: 5, borderRadius: 40 }} onPress={() => destroyData(item.id)}>
+											<Ionicons name="trash-outline" size={23} />
+										</Pressable>
+									</View>
+								</ScrollView>
+							)}
+						/>
+					</View>
+					<Pressable style={({ pressed }) => [pressed ? styles.floatingButton : styles.floatingButtonPressed]} onPress={() => openModal()}>
 						{({ pressed }) => <Ionicons name={pressed ? "create" : "create-outline"} size={pressed ? 23 : 28} style={{ color: "white" }} />}
 					</Pressable>
-					<Form modalVisible={modalVisible} setModalVisible={setModalVisible} getAllData={getAllData} />
+					<Form ref={modalRef} getAllData={getAllData} />
 				</View>
 			</SafeAreaView>
 		</>
-	);
-}
-
-function Form({ modalVisible, setModalVisible, getAllData }) {
-	const [field, setField] = useState({
-		task: "New Task",
-		description: "complete before end of the week",
-		usage: "work",
-		date_time: "2024-05-27T17:37:44",
-	});
-
-	const onChange = (event, selectedDate) => {
-		const currentDate = selectedDate;
-		setField({ ...field, date_time: currentDate });
-	};
-
-	const showMode = (currentMode) => {
-		DateTimePickerAndroid.open({
-			value: field.date_time,
-			onChange,
-			mode: currentMode,
-			is24Hour: true,
-		});
-	};
-
-	const showDatepicker = () => {
-		showMode("date");
-	};
-
-	const showTimepicker = () => {
-		showMode("time");
-	};
-
-	const storeData = async (value) => {
-		try {
-			const min = 1;
-			const max = 100;
-			const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-			const jsonValue = JSON.stringify(value);
-			await AsyncStorage.setItem(`task${randomNumber}`, jsonValue);
-		} catch (e) {
-			console.log("its error in store method.");
-		}
-	};
-
-	const submit = () => {
-		storeData(field);
-		closeModal();
-		getAllData();
-	};
-
-	const closeModal = () => {
-		setModalVisible(!modalVisible);
-		// setField({
-		// 	task: "",
-		// 	description: "",
-		// 	usage: "",
-		// 	date_time: "",
-		// });
-	};
-
-	return (
-		<Modal
-			animationType="slide"
-			transparent={true}
-			visible={modalVisible}
-			onRequestClose={() => {
-				setModalVisible(!modalVisible);
-			}}>
-			<View style={styles.centeredView}>
-				<View style={styles.modalView}>
-					<View style={styles.fieldBox}>
-						<Text style={styles.fieldTitle}>Task :</Text>
-						<TextInput
-							style={styles.textInput}
-							placeholder="What do you want to do?"
-							value={field.task}
-							onChangeText={(text) => {
-								setField({ ...field, task: text });
-							}}
-						/>
-					</View>
-					<View style={styles.fieldBox}>
-						<Text style={styles.fieldTitle}>Description :</Text>
-						<TextInput
-							editable
-							multiline
-							numberOfLines={4}
-							style={styles.textInput}
-							placeholder="Enter your description."
-							value={field.description}
-							onChangeText={(text) => {
-								setField({ ...field, description: text });
-							}}
-						/>
-					</View>
-					<View style={styles.fieldBox}>
-						<Text style={styles.fieldTitle}>Task For :</Text>
-						<Picker
-							style={styles.textInput}
-							selectedValue={field.usage}
-							onValueChange={(itemValue, itemIndex) => {
-								setField({ ...field, usage: itemValue });
-							}}>
-							<Picker.Item label="Personal" value="personal" />
-							<Picker.Item label="Work" value="work" />
-						</Picker>
-					</View>
-					<Text style={styles.fieldBox}>selected Date & Time : {field.date_time.toLocaleString()}</Text>
-					<View style={styles.fieldBox}>
-						<Text style={styles.fieldTitle}>Select Date :</Text>
-						<Pressable style={[styles.dateTimeButton]} onPress={showDatepicker}>
-							<Text style={{ textAlign: "center" }}>Select Date</Text>
-						</Pressable>
-					</View>
-					<View style={styles.fieldBox}>
-						<Text style={styles.fieldTitle}>Select Time :</Text>
-						<Pressable style={[styles.dateTimeButton]} onPress={showTimepicker}>
-							<Text style={{ textAlign: "center" }}>Select Time</Text>
-						</Pressable>
-					</View>
-					<Pressable style={[styles.button, { right: 90 }]} onPress={submit}>
-						<Text style={styles.textStyle}>Submit</Text>
-					</Pressable>
-					<Pressable style={[styles.button, { backgroundColor: "lightgrey" }]} onPress={closeModal}>
-						<Text style={[styles.textStyle, { color: "black" }]}>Cancel</Text>
-					</Pressable>
-				</View>
-			</View>
-		</Modal>
 	);
 }
 
@@ -212,74 +120,5 @@ const styles = StyleSheet.create({
 		position: "absolute",
 		bottom: 10,
 		right: 10,
-	},
-
-	centeredView: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		marginTop: 22,
-		height: "100%",
-	},
-
-	modalView: {
-		width: "98%",
-		height: "80%",
-		backgroundColor: "white",
-		borderRadius: 20,
-		padding: 30,
-		shadowColor: "#000",
-		shadowOffset: {
-			width: 0,
-			height: 2,
-		},
-		shadowOpacity: 0.25,
-		shadowRadius: 4,
-		elevation: 5,
-	},
-
-	button: {
-		padding: 10,
-		backgroundColor: "#2196F3",
-		alignItems: "center",
-		justifyContent: "center",
-		borderRadius: 20,
-		position: "absolute",
-		bottom: 20,
-		right: 15,
-	},
-
-	textStyle: {
-		color: "white",
-		fontWeight: "bold",
-		textAlign: "center",
-	},
-
-	fieldTitle: {
-		color: "grey",
-		marginLeft: 5,
-		fontSize: 16,
-	},
-
-	textInput: {
-		backgroundColor: "#fff2e4",
-		padding: 8,
-		marginTop: 10,
-		marginBottom: 10,
-		borderColor: "grey",
-		borderRadius: 10,
-	},
-
-	fieldBox: {
-		marginBottom: 10,
-	},
-
-	dateTimeButton: {
-		backgroundColor: "#fff2e4",
-		padding: 10,
-		width: 130,
-		borderRadius: 10,
-		marginTop: 5,
-		marginBottom: 5,
 	},
 });
